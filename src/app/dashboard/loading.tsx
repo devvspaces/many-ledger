56a318fcde4dbcebaa5091a814850e5069c30da3
Ayanwola Ayomide <77179231+devvspaces@users.noreply.github.com>
2025-03-19 23:54:50 +0100

    updte

--- src/app/dashboard/loading.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import PageLoadingBar from "@/components/loader"
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <PageLoadingBar />
-}
